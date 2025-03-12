6e6d9334f5caf7dd919b7a91d239004af4e2bac8
SuliemanHimad <suleiman.m@yooltech.com>
2025-03-12 11:23:55 +0300

    feat: Added Side bar

--- blog/views.py
+++ blog/views.py
@@ -3,23 +3,32 @@ from blog.models import Category,Post
 from django.core.paginator import Paginator
 # Create your views here.
 
+
+
+# Home View Function
 def HomeView(request):
     featuredPosts = Post.objects.order_by('-id')[0:3]
     post_lists  = Post.objects.order_by('-id')
+    categories = Category.objects.order_by("-id")
+
+
 
     paginator = Paginator(post_lists, 5)
     page = request.GET.get('page')
     post_lists = paginator.get_page(page)
 
 
-
-
     context = {
         "featuredPosts": featuredPosts,
-        "post_lists": post_lists
+        "post_lists": post_lists,
+        "categories": categories
     }
     return render(request, 'blog/home.html', context)
 
+
+
+
+# Category View Function
 def CategoryView(request):
     cat = Category.objects.order_by('-id')
     context ={
